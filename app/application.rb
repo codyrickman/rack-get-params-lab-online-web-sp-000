6db46151fd9343c6425ecc4fb1637672002fe667
Cody Rickman <cody@creativemindstudios.com>
2019-11-24 13:03:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,11 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      if (@@cart.length == 0)
+        resp.write("Your cart is empty")
+      else
       resp.write(@@cart)
+    end
     elsif req.path.match(/add/)
       item = req.params["items"]
       if (@@items.include?(item))
